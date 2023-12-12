5f08f42d2839c2e07832b2ff495175980c509d1f
dipu-mondal <dipu.ais.du@gmail.com>
2023-12-12 21:55:51 +0600

    js for today

--- jsFiles/home.js
+++ jsFiles/home.js
@@ -4,3 +4,36 @@
 // document.write('This is from document');
 // document.getElementById('navbar').innerHTML = 'Dipu';
 // alert('This is from alert');
+
+// function sleep(){
+//     console.log('Dipu is sleeping!!\n');
+// }
+// console.log('My name is Dipu Mondal.\n');
+// sleep();
+
+
+
+// -------------------------------------------------
+//function with parameter//
+//--------------------------------------------------
+function sleep(names, times){
+    console.log(names + ' is sleeping from '+ times + '\n');
+}
+sleep('Dipu Mondal', '8pm');
+sleep('Rita Biswas', '10pm');
+
+
+
+// -------------------------------------------------
+// Function with return value 
+//--------------------------------------------------
+function multiplication(firstVal, lastVal){
+    return firstVal * lastVal;
+}
+console.log(multiplication(10, 23));
+
+
+
+// -------------------------------------------------
+// Function with return value 
+//--------------------------------------------------
