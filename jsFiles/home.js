6402e41b3784e0dad1a0d199f5a3f39da4f7c71e
dipu-mondal <dipu.ais.du@gmail.com>
2023-12-15 00:08:51 +0600

    done

--- jsFiles/home.js
+++ jsFiles/home.js
@@ -13,67 +13,31 @@
 
 
 
-
-
-
 // -------------------------------------------------
 //function with parameter//
 //--------------------------------------------------
-// function sleep(names, times){
-//     console.log(names + ' is sleeping from '+ times + '\n');
-// }
-// sleep('Dipu Mondal', '8pm');
-// sleep('Rita Biswas', '10pm');
-
-
-
+function sleep(names, times){
+    console.log(names + ' is sleeping from '+ times + '\n');
+}
+sleep('Dipu Mondal', '8pm');
+sleep('Rita Biswas', '10pm');
 
 
 
 // -------------------------------------------------
 // Function with return value 
 //--------------------------------------------------
-// function multiplication(firstVal, lastVal){
-//     return firstVal * lastVal;
-// }
-// console.log(multiplication(10, 23));
-
-
-
+function multiplication(firstVal, lastVal){
+    return firstVal * lastVal;
+}
+console.log(multiplication(10, 23));
 
 
 
 // -------------------------------------------------
-// javascript object practice
+// Function 
 //--------------------------------------------------
-// const car = {
-//     car_name: 'Tata Maruti',
-//     model   : 'D850',
-//     weight  : '900kg',
-//     start   : function(){
-//         return this.car_name + ' has been started!';
-//     },
-//     stop    : function(){
-//         return this.car_name + ' has been stopped!';
-//     }
-// }
 
-// console.log(car.start());
-// console.log(car.stop());
-
-
-
-
-//-------------------------------------
-// JAVASCRIPT INTERPOLATION PRACTICE 
-//--------------------------------------
-
-// let person = 'Dipu Mondal';
-// let age  = 28;
-// let country = 'Bangladesh';
-
-// let statement = `My name is ${person}. I am ${age} years old and living in ${country} for ${age} years.`;
-// console.log(statement);
 
 
 
